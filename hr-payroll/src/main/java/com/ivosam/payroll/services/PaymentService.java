b4b83a37d8be5860bb2b022a7c55ea21aae75b31
ivosam1981 <ivgest@gmail.com>
2022-10-18 19:17:27 -0300

    rest template

--- hr-payroll/src/main/java/com/ivosam/payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/ivosam/payroll/services/PaymentService.java
@@ -1,14 +1,35 @@
 package com.ivosam.payroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.ivosam.payroll.entities.Payment;
+import com.ivosam.payroll.entities.Worker;
 
 @Service
 public class PaymentService {
 	
-	public Payment getPayment(long id, int days) {
-		return new Payment("Bob", 200.0, days); 
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	final RestTemplate template;
+	
+	public PaymentService(RestTemplate template) {
+		this.template = template;
+	}
+	
+	
+	public Payment getPayment(long workerId, int days) {
+		
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", String.valueOf(workerId));
+		
+		Worker worker = template.getForObject(workerHost+ "/workers/{id}", Worker.class, uriVariables);;
+		return new Payment(worker.getName(), worker.getDailyIncome(), days); 
 		
 	}
 
