1c829f4ff753b980b020db5343f6f752f080b877
ivosam1981 <ivgest@gmail.com>
2022-10-20 08:35:35 -0300

    hr-eureka-server-criado

---
+++ hr-eureka-server/src/test/java/com/ivosam/eurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivosam.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
