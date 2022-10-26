bca449719b4fb511644382f03c5a7620d782a9fd
ivosam1981 <ivgest@gmail.com>
2022-10-26 19:13:45 -0300

    database seeding

--- hr-user/src/main/java/com/ivosam/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/ivosam/hruser/HrUserApplication.java
@@ -1,16 +1,31 @@
 package com.ivosam.hruser;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication implements CommandLineRunner{
+	
+	final BCryptPasswordEncoder passwordEncoder;
+	
+	public HrUserApplication(BCryptPasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+//		System.out.println("BRCRYPT = " + passwordEncoder.encode("123456"));
+		
 	}
 
 }
