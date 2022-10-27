071a4ea63e9ed4e7e2fdbaf61ddb1df4d6dada7a
ivosam1981 <ivgest@gmail.com>
2022-10-27 12:08:38 -0300

    login e geracao do token JWT

--- hr-oauth/src/main/java/com/ivosam/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/ivosam/hroauth/services/UserService.java
@@ -2,13 +2,16 @@ package com.ivosam.hroauth.services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.ivosam.hroauth.entities.User;
 import com.ivosam.hroauth.feignclients.UserFeignClients;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 	
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
@@ -28,6 +31,17 @@ public class UserService {
 		logger.info("Email encontrado " + email);
 		return user;
 		
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClients.findByEmail(username).getBody();
+		if (user == null) {
+			logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("Email found: " + username);
+		return user;
 	}
 	
 	
